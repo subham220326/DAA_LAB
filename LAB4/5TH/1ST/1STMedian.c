71279e5a252b0020a2dfb589e86cd84bd009615e
Subham Mohanty <subham220326@gmail.com>
2026-08-29 11:53:26 +0530

    Add files via upload

---
+++ LAB4/5TH/1ST/1STMedian.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+int quickSelect(int arr[], int low, int high, int k) {
+    if (low <= high) {
+        int pi = partition(arr, low, high);
+        if (pi == k)
+            return arr[pi];
+        else if (pi > k)
+            return quickSelect(arr, low, pi - 1, k);
+        else
+            return quickSelect(arr, pi + 1, high, k);
+    }
+    return -1;
+}
+int findMedian(int arr[], int n) {
+    return quickSelect(arr, 0, n - 1, n / 2);
+}
+
+int main() {
+    int arr[] = {12, 3, 5, 7, 4, 19, 26};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\nMedian: %d\n", findMedian(arr, n));
+    return 0;
+}
